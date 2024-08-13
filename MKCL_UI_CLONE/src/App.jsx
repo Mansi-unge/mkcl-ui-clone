0da591e5717c884ff376ce117223bd595ccb1523
Mansi-unge <mansiunge@gmail.com>
2024-08-13 22:17:50 +0530

    react installed

---
+++ MKCL_UI_CLONE/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam sit voluptate alias, ipsam reiciendis ad dignissimos perspiciatis! Natus, non quae. Excepturi ducimus quis, enim perspiciatis sed est? Aut, ducimus nesciunt!</p>
+    </>
+  )
+}
+
+export default App
